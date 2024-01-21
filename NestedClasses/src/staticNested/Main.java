97be0a0d30c0a53ada265869a3a5199a9eaeea3c
yura <yurakim.ca23@gmail.com>
2024-01-21 16:52:11 +0900

    NestedClasses Static+NonStatic

---
+++ NestedClasses/src/staticNested/Main.java
@@ -0,0 +1,33 @@
+package staticNested;
+
+import staticNested.domain.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>(
+                //List.of()를 사용하면 element를 add, remove or modify 할 수 없다.
+                List.of(
+                        new Employee(10001, "Ralph", 2015),
+                        new Employee(10005, "Carole", 2021),
+                        new Employee(10022, "Jane", 2013),
+                        new Employee(13151, "Laura", 2020),
+                        new Employee(10050, "Jin", 2018)
+                )
+        );
+
+        //var comparator = new EmployeeComparator<>();
+        //employees.sort(comparator);
+
+        employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
+        for(Employee e : employees){
+            System.out.println(e);
+        }
+
+    }
+
+}
---
+++ NestedClasses/src/staticNested/domain/EmployeeComparator.java
@@ -0,0 +1,13 @@
+package staticNested.domain;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        //return o1.yearStarted - o2.yearStarted;
+        return o1.getName().compareTo(o2.getName());
+    }
+
+}//class
