a6f399bcf74bd102e6168d497073e1b6fd502645
yuraKIMyura <yurakim.ca23@gmail.com>
2024-02-06 17:46:57 +0900

    fill method not working

--- Collection/src/deckOfCards/Main.java
+++ Collection/src/deckOfCards/Main.java
@@ -1,5 +1,8 @@
 package deckOfCards;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -7,6 +10,21 @@ public class Main {
     public static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+
+        Card[] cardArray = new Card[13];
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+
+
+
     }//main
 
 }//class
