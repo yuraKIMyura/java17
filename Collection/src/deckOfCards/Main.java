0d02728f855f480bf88f13bb8c77a8d7cb2ce6ab
yuraKIMyura <yurakim.ca23@gmail.com>
2024-02-07 17:37:21 +0900

    Colletions methods

--- Collection/src/deckOfCards/Main.java
+++ Collection/src/deckOfCards/Main.java
@@ -1,9 +1,6 @@
 package deckOfCards;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -17,13 +14,39 @@ public class Main {
         Arrays.fill(cardArray, aceOfHearts);
         Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
 
+        //Creates an empty list. Sets the capacity to 52, doesn't populate the List with elements.
+        //fill() method will replace EXISTING elements, so if the List is empty, it won't be changed.
         List<Card> cards = new ArrayList<>(52);
         Collections.fill(cards, aceOfHearts);
         System.out.println(cards);
         System.out.println("cards.size() = " + cards.size());
 
+        List<Card> acesOfHearts = Collections.nCopies(13,aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
 
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
 
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card collections with Kings copied",2);
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck, "Shuffled Deck", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Reversed Deck of Cards", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank).thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "Standard Deck sorted by tan, suit", 13);
 
     }//main
 
