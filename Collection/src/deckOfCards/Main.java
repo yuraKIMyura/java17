6cff0c0ba05ba624459b88a77c3e10b197435f9d
yuraKIMyura <yurakim.ca23@gmail.com>
2024-02-08 17:42:59 +0900

    deck of cards slight addition

--- Collection/src/deckOfCards/Main.java
+++ Collection/src/deckOfCards/Main.java
@@ -48,6 +48,51 @@ public class Main {
         Collections.sort(deck, sortingAlgorithm);
         Card.printDeck(deck, "Standard Deck sorted by tan, suit", 13);
 
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Sorted by rank, suit reversed", 13);
+
+        List<Card> kings = new ArrayList<>(deck.subList(4, 8));
+        Card.printDeck(kings, "Kings in deck", 1);
+
+        List<Card> tens = new ArrayList<>(deck.subList(16, 20));
+        Card.printDeck(tens, "Tens in deck", 1);
+
+        Collections.shuffle(deck);
+        int subListIndex = Collections.indexOfSubList(deck, tens);
+        System.out.println("sublist index for tens = " + subListIndex);
+        System.out.println("ContainsAll = " + deck.containsAll(tens));
+
+        boolean disjoint = Collections.disjoint(deck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings, tens);
+        System.out.println("disjoint = " + disjoint2);
+
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32,36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32,36), "Tens row", 1);
+
+        if(Collections.replaceAll(deck, tenOfHearts, tenOfClubs)){
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        }else{
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " + Collections.frequency(deck, tenOfClubs));
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Wosrt Card = " + Collections.min(deck, sortingAlgorithm));
+
+
     }//main
 
 }//class
